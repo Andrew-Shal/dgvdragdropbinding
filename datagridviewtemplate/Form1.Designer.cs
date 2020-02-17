a1c41cdaefdeb8fc6aaa5e74ab714ba71241cc81
Andrew-Shal <shalandrew97@gmail.com>
2020-02-17 01:43:54 -0600

    initial commit

---
+++ datagridviewtemplate/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace datagridviewtemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TxtID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtFirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtLastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CmbPosition = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.employeeDGVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TxtID,
+            this.TxtFirstName,
+            this.TxtLastName,
+            this.CmbPosition});
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 365);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContextMenuStripNeeded += new System.Windows.Forms.DataGridViewCellContextMenuStripNeededEventHandler(this.dataGridView1_CellContextMenuStripNeeded);
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragOver += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragOver);
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDown);
+            this.dataGridView1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseMove);
+            // 
+            // TxtID
+            // 
+            this.TxtID.DataPropertyName = "EmployeeID";
+            this.TxtID.HeaderText = "EmployeeID";
+            this.TxtID.Name = "TxtID";
+            // 
+            // TxtFirstName
+            // 
+            this.TxtFirstName.DataPropertyName = "FirstName";
+            this.TxtFirstName.HeaderText = "First Name";
+            this.TxtFirstName.Name = "TxtFirstName";
+            // 
+            // TxtLastName
+            // 
+            this.TxtLastName.DataPropertyName = "LastName";
+            this.TxtLastName.HeaderText = "Last Name";
+            this.TxtLastName.Name = "TxtLastName";
+            // 
+            // CmbPosition
+            // 
+            this.CmbPosition.HeaderText = "Position";
+            this.CmbPosition.Name = "CmbPosition";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.employeeDGVToolStripMenuItem,
+            this.moveDownToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(138, 70);
+            // 
+            // employeeDGVToolStripMenuItem
+            // 
+            this.employeeDGVToolStripMenuItem.Name = "employeeDGVToolStripMenuItem";
+            this.employeeDGVToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.employeeDGVToolStripMenuItem.Text = "move up";
+            // 
+            // moveDownToolStripMenuItem
+            // 
+            this.moveDownToolStripMenuItem.Name = "moveDownToolStripMenuItem";
+            this.moveDownToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.moveDownToolStripMenuItem.Text = "move down";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.deleteToolStripMenuItem.Text = "delete";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(614, 29);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(695, 29);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtFirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtLastName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn CmbPosition;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem employeeDGVToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveDownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ datagridviewtemplate/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace datagridviewtemplate
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myConnection;
+        DataTable dtBS;
+
+        public Form1()
+        {
+            myConnection = new SqlConnection(@"server=DESKTOP-QPM5S0C\SQLEXPRESS;Trusted_Connection=yes;database=DGVDB;connection timeout=30");
+
+            InitializeComponent();
+
+            populateEmployeesTable();
+            populatePositionCombo();
+        }
+
+        private void populatePositionCombo() {
+            if (getPositions() != null)
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = getPositions();
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                DataGridViewComboBoxColumn comboColumn = (DataGridViewComboBoxColumn)dataGridView1.Columns["CmbPosition"];
+                comboColumn.DisplayMember = "positionName";
+                comboColumn.ValueMember = "positionID";
+                comboColumn.DataSource = dt;
+            }
+        }
+        private void populateEmployeesTable() {
+            if (getEmployees() != null) {
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = getEmployees();
+                dtBS = new DataTable();
+                da.Fill(dtBS);
+                dataGridView1.DataSource = dtBS;
+            }
+        }
+        private SqlCommand getPositions()
+        {
+            try
+            {
+                myConnection.Open();
+                string sql = @"SELECT [positionID],[positionName] FROM [dbo].[position]";
+                using (SqlCommand positions = new SqlCommand(sql, myConnection))
+                {
+                    myConnection.Close();
+                    return positions;
+                }
+            }
+            catch (Exception ex)
+            {
+                myConnection.Close();
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        private SqlCommand getEmployees() {
+            try {
+                myConnection.Open();
+                string sql = @"SELECT [EmployeeID],[FirstName],[LastName] FROM [dbo].[View_Employees]";
+                using (SqlCommand employees = new SqlCommand(sql, myConnection))
+                {
+                    myConnection.Close();
+                    return employees;
+                }
+            }
+            catch (Exception ex) {
+                myConnection.Close();
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        private void openConnection(SqlConnection myConn) {
+            try
+            {
+                myConn.Open();
+                MessageBox.Show("sucessfully connected!");
+            }
+            catch (Exception ex) {
+                myConnection.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContextMenuStripNeeded(object sender, DataGridViewCellContextMenuStripNeededEventArgs e)
+        {
+            //MessageBox.Show(e.RowIndex.ToString());
+        }
+
+
+        /// <summary>
+        /// DATA GRID DRAG AND DROP FUNCTIONALITY
+        /// </summary>
+        private Rectangle dragBoxFromMouseDown;
+        private int rowIndexFromMouseDown;
+        private int rowIndexOfItemUnderMouseToDrop;
+        private void dataGridView1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                // If the mouse moves outside the rectangle, start the drag.
+                if (dragBoxFromMouseDown != Rectangle.Empty &&
+                    !dragBoxFromMouseDown.Contains(e.X, e.Y))
+                {
+
+                    // Proceed with the drag and drop, passing in the list item.                    
+                    DragDropEffects dropEffect = dataGridView1.DoDragDrop(
+                    dataGridView1.Rows[rowIndexFromMouseDown],
+                    DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void refreshDataGrid()
+        {
+            dataGridView1.DataSource = dtBS;
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Get the index of the item the mouse is below.
+            rowIndexFromMouseDown = dataGridView1.HitTest(e.X, e.Y).RowIndex;
+            if (rowIndexFromMouseDown != -1)
+            {
+                // Remember the point where the mouse down occurred. 
+                // The DragSize indicates the size that the mouse can move 
+                // before a drag event should be started.                
+                Size dragSize = SystemInformation.DragSize;
+
+                // Create a rectangle using the DragSize, with the mouse position being
+                // at the center of the rectangle.
+                dragBoxFromMouseDown = new Rectangle(new Point(e.X - (dragSize.Width / 2),
+                                                               e.Y - (dragSize.Height / 2)),
+                                    dragSize);
+            }
+            else
+                // Reset the rectangle if the mouse is not over an item in the ListBox.
+                dragBoxFromMouseDown = Rectangle.Empty;
+        }
+
+        private void dataGridView1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            // The mouse locations are relative to the screen, so they must be 
+            // converted to client coordinates.
+            Point clientPoint = dataGridView1.PointToClient(new Point(e.X, e.Y));
+
+            // Get the row index of the item the mouse is below. 
+            rowIndexOfItemUnderMouseToDrop =
+                dataGridView1.HitTest(clientPoint.X, clientPoint.Y).RowIndex;
+
+            // If the drag operation was a move then remove and insert the row.
+            if (e.Effect == DragDropEffects.Move)
+            {
+                DataGridViewRow rowToMove = e.Data.GetData(
+                    typeof(DataGridViewRow)) as DataGridViewRow;
+
+                DataRow oldRow = ((DataRowView)rowToMove.DataBoundItem).Row;
+
+                DataRow newRow = dtBS.NewRow();
+                newRow.ItemArray = oldRow.ItemArray;
+                
+
+                dtBS.Rows.Remove(oldRow);
+                dtBS.Rows.InsertAt(newRow, rowIndexOfItemUnderMouseToDrop);
+                //dtBS.Rows.RemoveAt(rowIndexFromMouseDown+1);
+
+                if (rowIndexOfItemUnderMouseToDrop < 0)
+                {
+                    return;
+                }
+            }
+        }
+
+    }
+}
